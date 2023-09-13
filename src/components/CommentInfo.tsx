40756a6676d6a15dc3444f7c1bd7abbc143b71dd
Thien Le <thienle.luong@gmail.com>
2023-09-13 15:33:15 +0700

    change directory for images

--- src/components/CommentInfo.tsx
+++ src/components/CommentInfo.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CommentInfoProps extends React.HTMLAttributes<HTMLDivElement> {
   userName: string;
   userAvatar: string;
@@ -15,7 +17,7 @@ export default function CommentInfo({
   console.log('userAvatar', userAvatar)
   return (
     <div className={`flex gap-4 items-center col-span-2 ${className}`}>
-      <img className="w-8 h-8" src={userAvatar} alt="avatar" />
+      <Image width={32} height={32} src={userAvatar} alt="avatar" />
       <span className="text-heading-m text-dark-blue">{userName}</span>
       {isOwn && (
         <span className="bg-moderate-blue rounded-sm font-[13px] text-white px-[6px]">
